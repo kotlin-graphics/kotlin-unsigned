4d8a585f2f0147991a86ea4cb0f2fe707575dd42
elect <elect86@gmail.com>
2017-01-10 20:51:54 +0100

    kotlin gradle script

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+buildscript {
+
+    repositories {
+        gradleScriptKotlin()
+    }
+
+    dependencies {
+        classpath(kotlinModule("gradle-plugin"))
+    }
+}
+
+plugins {
+    application
+}
+
+apply {
+    plugin("kotlin")
+}
+
+configure<ApplicationPluginConvention> {
+ //   mainClassName = "samples.HelloWorldKt"
+}
+
+repositories {
+    gradleScriptKotlin()
+}
+
+dependencies {
+    compile(kotlinModule("stdlib"))
+}
