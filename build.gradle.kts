66841c12b29ddd952a7a7879cc2fe2bf900b3eae
elect <elect86@gmail.com>
2017-01-10 22:17:35 +0100

    kotlin gradle script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
+group = "groupId"
+version = "1.0-SNAPSHOT"
+
 buildscript {
 
     repositories {
+        maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap-1.1") }
+        mavenCentral()
         gradleScriptKotlin()
     }
 
@@ -17,14 +22,16 @@ apply {
     plugin("kotlin")
 }
 
-configure<ApplicationPluginConvention> {
- //   mainClassName = "samples.HelloWorldKt"
-}
+//configure<ApplicationPluginConvention> {
+//    mainClassName = "samples.HelloWorldKt"
+//}
 
 repositories {
+    maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap-1.1") }
     gradleScriptKotlin()
 }
 
 dependencies {
     compile(kotlinModule("stdlib"))
+    testCompile("io.kotlintest:kotlintest:1.3.5")
 }
