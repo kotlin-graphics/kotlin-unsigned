01aec2b2fa860a08c8399627a6f9aed629ef2558
Giuseppe Barbieri <elect86@gmail.com>
2017-01-12 10:46:10 +0100

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,7 @@
-import org.gradle.api.JavaVersion
-
-group = "groupId"
-version = "1.0-SNAPSHOT"
-
 buildscript {
 
     repositories {
         gradleScriptKotlin()
-        maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap-1.1") }
-        mavenCentral()
     }
 
     dependencies {
@@ -21,18 +14,8 @@ apply {
     plugin("maven")
 }
 
-//configure<ApplicationPluginConvention> {
-//    mainClassName = "samples.HelloWorldKt"
-//}
-
-//configure<JavaPluginConvention> {
-//    sourceCompatibility = JavaVersion.VERSION_1_7
-//    targetCompatibility = JavaVersion.VERSION_1_7
-//}
-
 repositories {
     gradleScriptKotlin()
-    maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap-1.1") }
 }
 
 dependencies {
