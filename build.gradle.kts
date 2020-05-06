1b9567eee508763b38d8bae55bd1237baf569a0e
Giuseppe Barbieri <elect86@gmail.com>
2020-05-06 10:11:28 +0200

    C:\Users\elect\IdeaProjects\kotlin-unsigned\src\main\java\module-info.java:4: error: module not found: kotlin.stdlib.jdk8

---
+++ build.gradle.kts
@@ -0,0 +1,133 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+val moduleName = "com.github.kotlin_graphics.kotlin_unsigned"
+val kot = "org.jetbrains.kotlin:kotlin"
+val kotlintest_version = "3.4.2"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+//java {
+//    withJavadocJar()
+//    withSourcesJar()
+//}
+
+//task sourcesJar(type: Jar, dependsOn: classes) {
+//    archiveClassifier = 'sources'
+//    from sourceSets.main.allSource
+//}
+//
+//task javadocJar(type: Jar, dependsOn: javadoc) {
+//    archiveClassifier = 'javadoc'
+//    from javadoc.destinationDir
+//}
+//
+//artifacts {
+//    archives sourcesJar
+////    archives javadocJar
+//}
+//
+
+tasks.compileKotlin {
+    // Enable Kotlin compilation to Java 8 class files with method parameter name metadata
+    kotlinOptions {
+        jvmTarget = "11"
+        freeCompilerArgs = listOf("-XXLanguage:+InlineClasses")
+//        javaParameters = true
+    }
+    // As per https://stackoverflow.com/a/47669720
+    // See also https://discuss.kotlinlang.org/t/kotlin-support-for-java-9-module-system/2499/9
+//    destinationDir = compileJava.destinationDir
+}
+
+tasks.compileTestKotlin {
+    kotlinOptions {
+        jvmTarget = "11"
+//        javaParameters = true
+    }
+}
+
+tasks.compileJava {
+    // this is needed because we have a separate compile step in this example with the 'module-info.java' is in 'main/java' and the Kotlin code is in 'main/kotlin'
+    options.compilerArgs = listOf("--patch-module", "$moduleName=${sourceSets.main.get().output.asPath}")
+}
+
+//compileJava {
+//    // this is needed because we have a separate compile step in this example with the 'module-info.java' is in 'main/java' and the Kotlin code is in 'main/kotlin'
+//    options.compilerArgs = ["--patch-module", "$moduleName=${sourceSets["main"].output.asPath}"]
+////    dependsOn(':compileKotlin')
+////    doFirst {
+////        options.compilerArgs = [
+////                '--module-path', classpath.asPath,
+////                '--patch-module', "$moduleName=${sourceSets["main"].output.asPath}"]
+////        classpath = files()
+////    }
+//}
+//
+//jar {
+//    inputs.property("moduleName", moduleName)
+////    manifest.attributes('Automatic-Module-Name': moduleName)
+////    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+//}
+//
+//test.useJUnitPlatform()
+//
+//plugins.withType(JavaPlugin).configureEach {
+//    java {
+//        modularity.inferModulePath = true
+//    }
+//}
+
+// == Add access to the 'modular' variant of kotlin("stdlib"): Put this into a buildSrc plugin and reuse it in all your subprojects
+configurations.all {
+    if (name.toLowerCase().endsWith("compileclasspath") || name.toLowerCase().endsWith("runtimeclasspath")) {
+        attributes.attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, objects.named("modular-jar"))
+    }
+    if (name.toLowerCase().endsWith("compile") || name.toLowerCase().endsWith("runtime")) {
+        isCanBeConsumed = false
+    }
+}
+
+dependencies {
+    attributesSchema.attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE).compatibilityRules.add(ModularJarCompatibilityRule::class)
+    components { withModule<ModularKotlinRule>(kotlin("stdlib")) }
+}
+
+abstract class ModularJarCompatibilityRule : AttributeCompatibilityRule<LibraryElements> {
+    override fun execute(details: CompatibilityCheckDetails<LibraryElements>): Unit = details.run {
+        if (producerValue?.name == LibraryElements.JAR && consumerValue?.name == "modular-jar")
+            compatible()
+    }
+}
+
+abstract class ModularKotlinRule : ComponentMetadataRule {
+
+    @javax.inject.Inject
+    abstract fun getObjects(): ObjectFactory
+
+    override fun execute(ctx: ComponentMetadataContext) {
+        val id = ctx.details.id
+        listOf("compile", "runtime").forEach { baseVariant ->
+            ctx.details.addVariant("${baseVariant}Modular", baseVariant) {
+                attributes {
+                    attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, getObjects().named("modular-jar"))
+                }
+                withFiles {
+                    removeAllFiles()
+                    addFile("${id.name}-${id.version}-modular.jar")
+                }
+                withDependencies {
+                    clear() // 'kotlin-stdlib-common' and  'annotations' are not modules and are also not needed
+                }
+            }
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.github.kotlin_graphics.kotlin_unsigned {
-    requires kotlin.stdlib;
-//    requires kotlin.stdlib.jdk8;
+
+//    requires kotlin.stdlib;
+    requires kotlin.stdlib.jdk8;
 
     exports unsigned;
 }
