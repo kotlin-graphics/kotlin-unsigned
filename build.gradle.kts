268e06dc97237c6365b84dd8c2e8429462dc3854
Giuseppe Barbieri <elect86@gmail.com>
2021-03-12 13:50:59 +0100

    jpms

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.4.31"
     //    val build = "0.6.4"
@@ -37,26 +39,36 @@ configurations.named(java9.implementationConfigurationName) {
     extendsFrom(configurations.implementation.get())
 }
 
-val moduleName = rootProject.run { "$group.$name" }
-val compileJava9 = tasks.named<JavaCompile>(java9.compileJavaTaskName) {
-    javaCompiler.set(javaToolchains.compilerFor {
-        languageVersion.set(JavaLanguageVersion.of(9))
-    })
-    options.compilerArgumentProviders.add(object : CommandLineArgumentProvider {
-        @get:Optional
-        @get:Classpath
-        protected val compileJavaOutputClasses = tasks.compileJava.flatMap { it.destinationDirectory }
-        override fun asArguments() = listOf(
-            "--patch-module",
-            "$moduleName=${tasks.compileJava.get().destinationDir.absolutePath}")
-    })
+val moduleName = "$group.$name"
+
+//val compileJava9 = tasks.named<JavaCompile>(java9.compileJavaTaskName) {
+//    javaCompiler.set(javaToolchains.compilerFor {
+//        languageVersion.set(JavaLanguageVersion.of(9))
+//    })
+//    options.compilerArgumentProviders.add(object : CommandLineArgumentProvider {
+//        @get:Optional
+//        @get:Classpath
+//        protected val compileJavaOutputClasses = tasks.compileJava.flatMap { it.destinationDirectory }
+//        override fun asArguments() = listOf(
+//            "--patch-module",
+//            "$moduleName=${tasks.compileJava.get().destinationDir.absolutePath}")
+//    })
+//}
+
+val compileKotlin9 = tasks.named<KotlinCompile>(java9.compileJavaTaskName) {
+    kotlinOptions.jvmTarget = "11"
+    sourceCompatibility = "11"
+    tasks.compileJava {
+        // this is needed because we have a separate compile step in this example with the 'module-info.java' is in 'main/java' and the Kotlin code is in 'main/kotlin'
+        options.compilerArgs = listOf("--patch-module", "$moduleName=${sourceSets.main.get().output.asPath}")
+    }
 }
 
 tasks.jar {
     manifest {
         attributes("Multi-Release" to true)
     }
-    from(compileJava9) {
+    from(compileKotlin9) {
         into("META-INF/versions/9/")
     }
 }
