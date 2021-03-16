170b4f83f30d3c0c59b5cf2b8ccead1976e0bb60
Giuseppe Barbieri <elect86@gmail.com>
2021-03-16 14:58:08 +0100

    v3.2.9+18

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,19 @@ plugins {
     id("kx.kotlin.11") version build
     id("kx.dokka") version build
     id("kx.publish") version "0.1.5"
+    `java-library`
 }
 
-version = "3.2.9+17"
+version = "3.2.9+18"
+
+//publishing {
+////     default in java-library
+////        publications.create<MavenPublication>("maven") {
+////            from(components["java"])
+////        }
+////    repositories {
+////        maven {
+////            url = uri("$rootDir/../mary")
+////        }
+////    }
+//}
