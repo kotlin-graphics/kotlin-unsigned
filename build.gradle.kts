b51c12a07e149045e071102c4faa2deb2bf914c2
Giuseppe Barbieri <elect86@gmail.com>
2021-03-16 14:55:00 +0100

    v3.2.9+17

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     val build = "0.7.0+27"
     id("kx.kotlin.11") version build
     id("kx.dokka") version build
-    id("kx.publish") version "0.1.2"
+    id("kx.publish") version "0.1.5"
 }
 
-version = "3.2.9+16"
+version = "3.2.9+17"
