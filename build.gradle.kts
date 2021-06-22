6be5514053910e281a66e9bfd958757e0350611d
Giuseppe Barbieri <elect86@gmail.com>
2021-06-22 12:51:15 +0200

    up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -7,6 +8,8 @@ plugins {
     //    id("kx.dokka") version build
     //    id("kx.jitpack") version build
     java
+    `java-library`
+    `maven-publish`
 }
 
 group = "kotlin.graphics"
@@ -24,51 +27,80 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-core:4.4.1")
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
+val jdk8 = sourceSets["main"] // default is fine
+
+val jdk11 = sourceSets.create("jpms") {
+    java.srcDir("src/main/java")
+}
+
+java.registerFeature("jdk8") {
+    usingSourceSet(jdk8)
+    capability(project.group.toString(), project.name, project.version.toString())
+}
+
+jdk8 configureCompileVersion 8
+jdk11 configureCompileVersion 11
+
+infix fun SourceSet.configureCompileVersion(jdkVersion: Int) {
+    //    println("sourceset: ${this@configureCompileVersion.compileJavaTaskName}")
+    //    println("sourceset: ${this@configureCompileVersion.compileKotlinTaskName}")
+    //        tasks.withType<KotlinJvmCompile> {
+    //            println(this.name)
+    //        }
+    //    tasks.withType<JavaCompile> { println(this.name) }
+    //    tasks.withType<KotlinJvmCompile> { println(this.name) }
+    //    tasks.named<KotlinJvmCompile>("compileKotlin") {
+    //        println(kotlinOptions.jdkHome)
+    //    }
+    //    tasks.forEach { println("[${it.name}]:${it.javaClass}") }
+    //    tasks.named<KotlinJvmCompile>(compileKotlinTaskName) {
+    //        val compiler = javaToolchains.compilerFor {
+    //            languageVersion.set(JavaLanguageVersion.of(javaVersion))
+    //        }.get()
+    //        kotlinOptions.jdkHome = compiler.metadata.installationPath.asFile.absolutePath
+    //    }
+    tasks {
+        named<KotlinCompile>(compileKotlinTaskName) {
+            val compiler = project.javaToolchains.compilerFor {
+                languageVersion.set(JavaLanguageVersion.of(jdkVersion))
+            }.get()
+            kotlinOptions.jdkHome = compiler.metadata.installationPath.asFile.absolutePath
+            source = sourceSets.main.get().kotlin
+        }
+        named<JavaCompile>(compileJavaTaskName) {
+            javaCompiler.set(project.javaToolchains.compilerFor {
+                languageVersion.set(JavaLanguageVersion.of(jdkVersion))
+            })
+            source = sourceSets.main.get().java + java
+        }
     }
-    modularity.inferModulePath.set(true)
 }
-val java9 by sourceSets.creating {
-    java {
-        setSrcDirs(listOf("src/main/java9"))
-    }
-}
-configurations.named(java9.implementationConfigurationName) {
-    extendsFrom(configurations.implementation.get())
-}
+
+val SourceSet.compileKotlinTaskName: String
+    get() = getCompileTaskName("kotlin")
+
+val SourceSet.kotlin: SourceDirectorySet
+    get() = withConvention(KotlinSourceSet::class) { kotlin }
 
 val moduleName = "$group.$name"
 
-//val compileJava9 = tasks.named<JavaCompile>(java9.compileJavaTaskName) {
-//    javaCompiler.set(javaToolchains.compilerFor {
-//        languageVersion.set(JavaLanguageVersion.of(9))
-//    })
-//    options.compilerArgumentProviders.add(object : CommandLineArgumentProvider {
-//        @get:Optional
-//        @get:Classpath
-//        protected val compileJavaOutputClasses = tasks.compileJava.flatMap { it.destinationDirectory }
-//        override fun asArguments() = listOf(
-//            "--patch-module",
-//            "$moduleName=${tasks.compileJava.get().destinationDir.absolutePath}")
-//    })
-//}
-
-val compileKotlin9 = tasks.named<KotlinCompile>(java9.compileJavaTaskName) {
-    kotlinOptions.jvmTarget = "11"
-    sourceCompatibility = "11"
-    tasks.compileJava {
-        // this is needed because we have a separate compile step in this example with the 'module-info.java' is in 'main/java' and the Kotlin code is in 'main/kotlin'
-        options.compilerArgs = listOf("--patch-module", "$moduleName=${sourceSets.main.get().output.asPath}")
-    }
+tasks.compileJava {
+    // this is needed because we have a separate compile step in this example with the 'module-info.java' is in 'main/java' and the Kotlin code is in 'main/kotlin'
+    options.compilerArgs = listOf("--patch-module", "org.module.kotlin=${sourceSets.main.get().output.asPath}")
 }
 
-tasks.jar {
-    manifest {
-        attributes("Multi-Release" to true)
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "org.gradle.sample"
+            artifactId = "library"
+            version = "1.1"
+
+            from(components["java"])
+        }
     }
-    from(compileKotlin9) {
-        into("META-INF/versions/9/")
+    repositories.maven {
+        name = "prova"
+        url = uri("repo")
     }
 }
