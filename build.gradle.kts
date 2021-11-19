08830716eb6d65cc837ebb8bab3690bc1f5877c3
Giuseppe Barbieri <elect86@gmail.com>
2021-11-19 11:58:19 +0100

    3.3.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     fun kx(vararg p: Pair<String, String>) = p.forEach { id("io.github.kotlin-graphics.${it.first}") version it.second }
-    kx("base" to "0.0.4",
-       "publish" to "0.0.2",
-       "utils" to "0.0.2")
+    kx("base" to "0.0.9",
+       "publish" to "0.0.5",
+       "utils" to "0.0.5")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,11 +5,12 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://raw.githubusercontent.com/kotlin-graphics/mary/master")
     }
+//    includeBuild("../build-logic")
 }
 
 gradle.rootProject {
     group = "kotlin.graphics"
-    version = "3.3.1+5"
+    version = "3.3.3"
 }
 
 //includeBuild("../magik")
