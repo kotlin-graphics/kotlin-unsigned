20986d016718b8d41864c71781af66429b25d988
Giuseppe Barbieri <elect86@gmail.com>
2022-11-01 14:41:22 +0100

    3.3.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,45 @@
+import magik.createGithubPublication
+import magik.github
+import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
+
 plugins {
-    fun kx(vararg p: Pair<String, String>) = p.forEach { id("io.github.kotlin-graphics.${it.first}") version it.second }
-    kx("base" to "0.0.9",
-       "publish" to "0.0.5",
-       "utils" to "0.0.5")
+    kotlin("jvm") version embeddedKotlinVersion
+    id("elect86.magik") version "0.3.1"
+    `maven-publish`
 }
+
+repositories { mavenCentral() }
+
+dependencies {
+    testImplementation("io.kotest:kotest-runner-junit5:5.4.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.4.1")
+}
+
+kotlin.jvmToolchain {
+    this as JavaToolchainSpec
+    languageVersion.set(JavaLanguageVersion.of(8))
+}
+
+tasks {
+    withType<KotlinCompile<*>>().all {
+        kotlinOptions {
+            freeCompilerArgs += listOf("-opt-in=kotlin.RequiresOptIn")
+        }
+    }
+}
+
+publishing {
+    publications {
+        createGithubPublication {
+            from(components["java"])
+            suppressAllPomMetadataWarnings()
+        }
+    }
+    repositories {
+        github {
+            domain = "kotlin-graphics/mary"
+        }
+    }
+}
+
+java { withSourcesJar() }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
 
 gradle.rootProject {
     group = "kotlin.graphics"
-    version = "3.3.3"
+    version = "3.3.31"
 }
 
 //includeBuild("../magik")
