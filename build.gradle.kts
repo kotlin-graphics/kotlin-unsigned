12461f50a355a0b24bd089799c3794b1085f0ed4
Bixilon <bixilon@bixilon.de>
2023-06-03 20:17:13 +0200

    bump kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,8 @@ plugins {
 repositories { mavenCentral() }
 
 dependencies {
-    testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
-    testImplementation("io.kotest:kotest-assertions-core:5.5.5")
+    testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.6.2")
 }
 
 kotlin.jvmToolchain { languageVersion.set(JavaLanguageVersion.of(8)) }
